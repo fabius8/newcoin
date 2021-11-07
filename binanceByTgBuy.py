35274da4ce16cda53f536053aeb6973f249fee37
FANG <fabius8@163.com>
2021-11-07 20:51:48 +0800

    Update binanceByTgBuy.py

--- binanceByTgBuy.py
+++ binanceByTgBuy.py
@@ -3,21 +3,55 @@ import json
 import ccxt
 import re
 import requests
+import time
 from datetime import datetime
 
 config = json.load(open('config.json'))
 gateio = {}
 okex = {}
 mexc = {}
+gateio['usdtQuantity'] = 5000
+mexc['usdtQuantity'] = 2000
+okex['usdtQuantity'] = 500
 
 def gateioInit(config):
     global gateio
     gateio["spot"] = ccxt.gateio(config["gate"])
     gateio["spot"].load_markets()
 
+# gate sell
+def gateioSell(coin, gateio):
+    quantity = 0
+    request = {}
+    accounts = gateio["spot"].privateSpotGetAccounts(request)
+    for account in accounts:
+        if account['currency'] == coin:
+            quantity = account["available"]
+            break
+    pair = coin + "_USDT"
+    request = {
+        'currency_pair': pair,
+    }
+    price = float(gateio["spot"].publicSpotGetOrderBook(request)['bids'][9][0])
+
+    request = {
+        'currency_pair': pair,
+        'amount': gateio["spot"].amount_to_precision(pair, quantity),
+        'price': gateio["spot"].price_to_precision(pair, price),
+        'side': "sell",
+    }
+    try:
+        gateio["spot"].privateSpotPostOrders(request)
+    except Exception as e:
+        print(datetime.now().strftime("%Y-%m-%d %H:%M:%S"), e)
+        pass
+        return
+    return request
+
+# gate buy
 def gateioTrade(coin, gateio):
     gateio["spot"].load_markets()
-    usdtQuantity = 3500
+    usdtQuantity = gateio['usdtQuantity']
     pair = coin + "_USDT"
     request = {
         'currency_pair': pair,
@@ -39,7 +73,7 @@ def okexInit(config):
 
 def okexTrade(coin, okex):
     okex["spot"].load_markets()
-    usdtQuantity = 500
+    usdtQuantity = okex['usdtQuantity']
     pair = coin + "-USDT"
     request = {
         "instId": pair,
@@ -71,7 +105,7 @@ def mexcInit(config):
 
 def mexcTrade(coin, mexc):
     mexc["spot"].load_markets()
-    usdtQuantity = 700
+    usdtQuantity = mexc['usdtQuantity']
     pair = coin + "_USDT"
     data = get_depth(pair, 10)
     price = float(data["data"]["asks"][9]["price"])
@@ -80,6 +114,22 @@ def mexcTrade(coin, mexc):
         'price': mexc["spot"].price_to_precision(pair, price),
         'quantity': mexc["spot"].amount_to_precision(pair, usdtQuantity/price),
         'trade_type': 'BID',
+        'order_type': 'LIMIT_ORDER'
+    }
+    mexc["spot"].spotPrivatePostOrderPlace(request)
+    return request
+# Mexc Sell
+def mexcSell(coin, mexc):
+    account = mexc["spot"].spotPrivateGetAccountInfo({})
+    quantity = account['data'][coin]['available']
+    pair = coin + "_USDT"
+    data = get_depth(pair, 10)
+    price = float(data["data"]["bids"][9]["price"])
+    request = {
+        'symbol': pair,
+        'price': mexc["spot"].price_to_precision(pair, price),
+        'quantity': mexc["spot"].amount_to_precision(pair, quantity),
+        'trade_type': 'ASK',
         'order_type': 'LIMIT_ORDER'
     }
     mexc["spot"].spotPrivatePostOrderPlace(request)
@@ -136,10 +186,34 @@ def result():
             r = okexTrade(coin, okex)
             print(datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
             text = "okex" + " BUY OK\n" + str(r) + "\n"
-            print(text)  
+            print(text)
         except Exception as err:
             print(datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
             text = "okex" + " MISS!" + "\n"
+            print(text, err)
+            pass
+
+        # SELL
+        time.sleep(3)
+        try:
+            r = gateioSell(coin, gateio)
+            print(datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
+            text = "Gate.io" + " SELL OK\n" + str(r) + "\n"
+            print(text)
+        except Exception as err:
+            print(datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
+            text = "Gate.io" + " SELL FAIL!" + "\n"
+            print(text, err)
+            pass
+
+        try:
+            r = mexcSell(coin, mexc)
+            print(datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
+            text = "Mexc" + " SELL OK\n" + str(r) + "\n"
+            print(text)
+        except Exception as err:
+            print(datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
+            text = "Mexc" + " SELL FAIL!" + "\n"
             print(text, err)
             pass
 
